ee4bb702c9119b62459f5bc40fbe7c1e174e7958
Adam Rice <development@hashnotadam.com>
2017-06-19 16:33:54 +1000

    :cop: Use underscore for unused block parameters

--- spec/restroom_spec.rb
+++ spec/restroom_spec.rb
@@ -29,9 +29,9 @@ module Scifi
 
     restroom 'https://scifi.org', base_path: 'api' do
       exposes :authors do
-        exposes :influences, model: Author, response_filter: proc { |mode, data| data['influences'] }
+        exposes :influences, model: Author, response_filter: proc { |_, data| data['influences'] }
         exposes :titles, model: Book, resource: :books, id: :key do
-          response_filter proc { |mode, data| data['data'] }
+          response_filter proc { |_, data| data['data'] }
         end
       end
     end.dump
